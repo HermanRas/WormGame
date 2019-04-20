5c179518ec5763a85cc969b9a2c8c7dd0c221921
Herman Ras <hermanr@PetraGroup.local>
2019-04-20 08:17:26 +0200

    Broken

--- js/game.js
+++ js/game.js
@@ -1,9 +1,52 @@
-function setup() {
-    createCanvas(400, 400);
+let fps = 4;
+let rez = 20;
+let food;
+let w, h;
 
+function setup() {
+    frameRate(fps);
+    createCanvas(400, 400);
+    w = floor(width / rez);
+    h = floor(height / rez);
+    snake = new snake();
+    foodLocation();
+}
+
+function foodLocation() {
+    let x = floor(random(w));
+    let y = floor(random(h));
+    food = createVector(x, y);
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        snake.setDir(-1, 0);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.setDir(1, 0);
+    } else if (keyCode === UP_ARROW) {
+        snake.setDir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.setDir(0, 1);
+    } else if (key == ' ') {
+        snake.grow();
+    }
 }
 
 function draw() {
+    if (snake.endGame()) {
+        background(255, 0, 0);
+        noLoop();
+    }
 
-    console.log("ola");
+    scale(rez);
+    background(220);
+    snake.update();
+    snake.show();
+    if (snake.eatFood(food)) {
+        foodLocation();
+    }
+
+    noStroke();
+    fill(0, 255, 0);
+    rect(food.x, food.y, 1, 1);
 }
