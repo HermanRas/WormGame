82f34881e9ea348e386eae108b4710560cc09c97
Herman Ras <hermanr@PetraGroup.local>
2019-04-23 07:42:36 +0200

    Updated Images

--- game.php
+++ game.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
     <script src="js/p5.min.js"></script>
     <script src="js/p5.dom.min.js"></script>
     <script src="js/food.js"></script>
